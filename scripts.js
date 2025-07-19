fe5ba206bc9d1606701c55365aa9ac735f269864
Jennifer Gibbs <jennegibbs@gmail.com>
2025-07-19 18:49:37 +1000

    Adding API informatin and creating function for updating city and temperature

--- scripts.js
+++ scripts.js
@@ -1,17 +1,21 @@
-function updateWeather(response) {}
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature-current");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+}
 
-function searchForCity(city) {
+function searchCity(city) {
   let apiKey = "7fftc541dbabc3018759foaa254e09a2";
-  let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(updateWeather);
+  console.log(apiUrl);
 }
 
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
