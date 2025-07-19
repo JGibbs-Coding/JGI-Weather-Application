71da6bf9d020c309d1dda30a77930e95a05aa8ad
Jennifer Gibbs <jennegibbs@gmail.com>
2025-07-19 14:24:06 +1000

    Added Axios

--- scripts.js
+++ scripts.js
@@ -1,9 +1,21 @@
+function updateWeather(response) {}
+
+function searchForCity(city) {
+  let apiKey = "7fftc541dbabc3018759foaa254e09a2";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}";
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
+
+searchCity("Sydney");
